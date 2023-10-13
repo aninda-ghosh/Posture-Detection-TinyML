3cea1dcd5602f1bfbb06f38825712ee2deee51df
Aninda Ghosh <anindaghosh63@gmail.com>
2023-10-12 19:32:26 -0700

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Posture-Detection-TinyML
+
+## Scope of Work
+- Collection of IMU data (Labeled)
+- Pre-Process IMU data to produce window of 200 to create contextual window for the time series data.
+- 
+
+## Workflow
+
+### Step 1: Capturing Data
+
+### Step 2: Data Pre-Processing
+
+### Step 3: Model Architecture
+
+### Step 4: Conversion of PyTorch/Tensorflow to Tensorflow Lite
+
+### Step 5: Arduino Deployment
+
+## Technologies Used
--- _posture_detection_model.py
+++ _posture_detection_model.py
@@ -2,20 +2,95 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-class PostureDetectionModel(keras.Model):
+class PostureDetectionModel_GRU(keras.Model):
     def __init__(self, features, sequence_per_feature, classes):
         super().__init__()
         self.hidden_dimension = 80
 
-        self.sens_seq_model =   keras.Sequential([
-                                    keras.layers.GRU(self.hidden_dimension, input_shape=(sequence_per_feature, features)),
-                                    keras.layers.Dense(40, activation='relu'),
-                                    keras.layers.Dropout(0.65),
-                                    keras.layers.Dense(classes)
-                                ])
+        self.input0_0 = keras.Input(shape=(200, 3, ))
+
+        self.gru1_0  = keras.layers.GRU(self.hidden_dimension, return_sequences=True)(self.input0_0[:, :, 0])
+        self.gru1_1  = keras.layers.GRU(self.hidden_dimension, return_sequences=True)(self.input0_0[:, :, 1])
+        self.gru1_2  = keras.layers.GRU(self.hidden_dimension, return_sequences=True)(self.input0_0[:, :, 2])
+
+        self.linear2_0  = keras.layers.Dense(60, activation='tanh')(tf.concat([self.gru1_0, self.gru1_1, self.gru1_2], axis=-1))
+
+        self.dropout3_0 = keras.layers.Dropout(0.30)(self.linear2_0)
+
+        self.linear4_0  = keras.layers.Dense(classes)(self.dropout3_0)
+
+        self.sigmoid5_0 = keras.layers.Activation('sigmoid')(self.linear4_0)
+
+        self.model      = keras.models.Model(inputs=self.input0_0, outputs=self.sigmoid5_0)
 
     def call(self, x, training=False):
-        x = self.sens_seq_model(x, training=training)
-        x = tf.sigmoid(x)
+        # Set the training mode for the dropout layers based on the 'training' argument
+        self.dropout3_0.training = training
 
+        x = self.model(x, training=training)
+        return x
+    
+class PostureDetectionModel_LSTM(keras.Model):
+    def __init__(self, features, sequence_per_feature, classes):
+        super().__init__()
+        self.hidden_dimension = 80
+
+        self.input0_0 = keras.Input(shape=(200, 3, ))
+
+        self.lstm1_0  = keras.layers.LSTM(self.hidden_dimension, return_sequences=True)(self.input0_0[:, :, 0])
+        self.lstm1_1  = keras.layers.LSTM(self.hidden_dimension, return_sequences=True)(self.input0_0[:, :, 1])
+        self.lstm1_2  = keras.layers.LSTM(self.hidden_dimension, return_sequences=True)(self.input0_0[:, :, 2])
+
+        self.linear2_0  = keras.layers.Dense(60, activation='tanh')(tf.concat([self.lstm1_0, self.lstm1_1, self.lstm1_2], axis=-1))
+
+        self.dropout3_0 = keras.layers.Dropout(0.30)(self.linear2_0)
+
+        self.linear4_0  = keras.layers.Dense(classes)(self.dropout3_0)
+
+        self.sigmoid5_0 = keras.layers.Activation('sigmoid')(self.linear4_0)
+
+        self.model      = keras.models.Model(inputs=self.input0_0, outputs=self.sigmoid5_0)
+
+    def call(self, x, training=False):
+        # Set the training mode for the dropout layers based on the 'training' argument
+        self.dropout3_0.training = training
+
+        x = self.model(x, training=training)
+        return x
+    
+
+class PostureDetectionModel_Dense(keras.Model):
+    def __init__(self, classes):
+        super().__init__()
+        self.hidden_dimension = 80
+        
+        self.input0_0 = keras.Input(shape=(200, 3, ))
+        
+        self.linear1_0  = keras.layers.Dense(40, activation='relu')(self.input0_0[:, :, 0])
+        self.dropout1_0 = keras.layers.Dropout(0.20)(self.linear1_0)
+
+        self.linear1_1  = keras.layers.Dense(40, activation='relu')(self.input0_0[:, :, 1])
+        self.dropout1_1 = keras.layers.Dropout(0.20)(self.linear1_1)
+
+        self.linear1_2  = keras.layers.Dense(40, activation='relu')(self.input0_0[:, :, 2])
+        self.dropout1_2 = keras.layers.Dropout(0.20)(self.linear1_2)
+
+        self.linear2_0  = keras.layers.Dense(60, activation='tanh')(tf.concat([self.dropout1_0, self.dropout1_1, self.dropout1_2], axis=-1))
+        
+        self.dropout3_0 = keras.layers.Dropout(0.30)(self.linear2_0)
+        
+        self.linear4_0  = keras.layers.Dense(classes)(self.dropout3_0)
+        
+        self.sigmoid5_0 = keras.layers.Activation('sigmoid')(self.linear4_0)
+        
+        self.model      = keras.models.Model(inputs=self.input0_0, outputs=self.sigmoid5_0)
+
+    def call(self, x, training=False):
+        # Set the training mode for the dropout layers based on the 'training' argument
+        self.dropout1_0.training = training
+        self.dropout1_1.training = training
+        self.dropout1_2.training = training
+        self.dropout3_0.training = training
+
+        x = self.model(x, training=training)
         return x
