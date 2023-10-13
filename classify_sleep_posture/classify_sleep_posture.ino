abb7d4ec0b672da63d464f63b61fbf6b0b4d4b43
Aninda Ghosh <anindaghosh63@gmail.com>
2023-10-12 19:33:47 -0700

    Updated Arduino Code

--- classify_sleep_posture/classify_sleep_posture.ino
+++ classify_sleep_posture/classify_sleep_posture.ino
@@ -1,11 +1,14 @@
 #include <Arduino_LSM9DS1.h>
+
 #include <TensorFlowLite.h>
 #include <tensorflow/lite/micro/all_ops_resolver.h>
 #include <tensorflow/lite/micro/micro_error_reporter.h>
 #include <tensorflow/lite/micro/micro_interpreter.h>
 #include <tensorflow/lite/schema/schema_generated.h>
 #include <tensorflow/lite/version.h>
+
 #include "iirfilter.h"
+#include "model.h"
 
 IIRFILTER butterworth_ax;
 IIRFILTER butterworth_ay;
@@ -18,11 +21,30 @@ IIRFILTER butterworth_my;
 IIRFILTER butterworth_mz;
 
 float ax, ay, az, gx, gy, gz, mx, my, mz;
-float filt_ax, filt_ay, filt_az, filt_gx, filt_gy, filt_gz, filt_mx, filt_my, filt_mz;  
+float filt_ax, filt_ay, filt_az, filt_gx, filt_gy, filt_gz, filt_mx, filt_my, filt_mz;
 int window_start = 0;
 int print_time = 0;
 char sensor2use = 'a';
 
+// Array to map gesture index to a name
+const char* GESTURES[] = {
+  "Supine", "Prone", "Side", "Sitting", "Unknown"
+};
+
+
+//==============================================================================
+// Capture variables
+//==============================================================================
+#define NUM_SAMPLES 200
+#define MOTION_THRESHOLD 0.0
+#define CAPTURE_DELAY 10  // This is now in milliseconds
+#define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
+
+bool isPredicting = false;
+
+// Num samples read from the IMU sensors
+// "Full" by default to start in idle
+int numSamplesRead = 0;
 
 //==============================================================================
 // TensorFlow variables
@@ -31,7 +53,7 @@ char sensor2use = 'a';
 // Global variables used for TensorFlow Lite (Micro)
 tflite::MicroErrorReporter tflErrorReporter;
 
-// Auto resolve all the TensorFlow Lite for MicroInterpreters ops, for reduced memory-footprint change this to only 
+// Auto resolve all the TensorFlow Lite for MicroInterpreters ops, for reduced memory-footprint change this to only
 // include the op's you need.
 tflite::AllOpsResolver tflOpsResolver;
 
@@ -43,15 +65,16 @@ TfLiteTensor* tflOutputTensor = nullptr;
 
 // Create a static memory buffer for TensorFlow Lite for MicroInterpreters, the size may need to
 // be adjusted based on the model you are using
-constexpr int tensorArenaSize = 100 * 1024;
+constexpr int tensorArenaSize = 30 * 1024;
 byte tensorArena[tensorArenaSize];
 
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(9600);
   while (!Serial)
     ;
-  Serial.println("Started");
 
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
@@ -72,10 +95,11 @@ void setup() {
   Serial.println(" Hz");
 
   // Get the TFL representation of the model byte array
-  tflModel = tflite::GetModel(model);
+  tflModel = tflite::GetModel(posture_detection_model_tflite);
   if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
     Serial.println("Model schema mismatch!");
-    while (1);
+    while (1)
+      ;
   }
 
   // Create an interpreter to run the model
@@ -88,63 +112,126 @@ void setup() {
   tflInputTensor = tflInterpreter->input(0);
   tflOutputTensor = tflInterpreter->output(0);
 
-  delay(3000);
+  Serial.println("\nSelect Sensor Menu");
+  Serial.println("a -> accelerometer");
+  Serial.println("g -> Gyroscope");
+  Serial.println("m -> Magnetometer");
 }
 
 void loop() {
-  if(Serial.available()){
-    sensor2use = Serial.read();
-  }
-
-  if (IMU.accelerationAvailable()) { 
-    IMU.readAcceleration(ax, ay, az);
-    filt_ax = butterworth_ax.filter(ax);
-    filt_ay = butterworth_ay.filter(ay);
-    filt_az = butterworth_az.filter(az); 
-  }
-
-  if (IMU.gyroscopeAvailable()) {
-    IMU.readGyroscope(gx, gy, gz);
-    filt_gx = butterworth_gx.filter(gx);
-    filt_gy = butterworth_gy.filter(gy);
-    filt_gz = butterworth_gz.filter(gz); 
-  }
-
-  if (IMU.magneticFieldAvailable()) {
-    IMU.readMagneticField(mx, my, mz);
-    filt_mx = butterworth_mx.filter(mx);
-    filt_my = butterworth_my.filter(my);
-    filt_mz = butterworth_mz.filter(mz); 
-  }
-
-  switch (sensor2use) {
-    case 'a' : {
-      Serial.print(filt_ax);
-      Serial.print(',');
-      Serial.print(filt_ay);
-      Serial.print(',');
-      Serial.println(filt_az);
-      break;
-    }
-
-    case 'g' : {
-      Serial.print(filt_gx);
-      Serial.print(',');
-      Serial.print(filt_gy);
-      Serial.print(',');
-      Serial.println(filt_gz);
-      break;
-    }
-    
-    case 'm' : {
-      Serial.print(filt_mx);
-      Serial.print(',');
-      Serial.print(filt_my);
-      Serial.print(',');
-      Serial.println(filt_mz);
-      break;
+  if (!isPredicting) {
+    if (Serial.available()) {
+      sensor2use = Serial.read();
+      Serial.print("\nSelecting Mode: ");
+      switch (sensor2use) {
+        case 'a':
+          {
+            Serial.println("Accelerometer");
+            break;
+          }
+        case 'g':
+          {
+            Serial.println("Gyroscope");
+            break;
+          }
+        case 'm':
+          {
+            Serial.println("Magnetometer");
+            break;
+          }
+      }
+      isPredicting = true;
+      numSamplesRead = 0;  // Reset the window index
+      Serial.print("Capturing Data: ");
     }
   }
 
-  delay(10);
+  if (isPredicting) {
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) {
+      IMU.readAcceleration(ax, ay, az);
+      IMU.readGyroscope(gx, gy, gz);
+      IMU.readMagneticField(mx, my, mz);
+
+      filt_ax = butterworth_ax.filter(ax);
+      filt_ay = butterworth_ay.filter(ay);
+      filt_az = butterworth_az.filter(az);
+
+      filt_gx = butterworth_gx.filter(gx);
+      filt_gy = butterworth_gy.filter(gy);
+      filt_gz = butterworth_gz.filter(gz);
+
+      filt_mx = butterworth_mx.filter(mx);
+      filt_my = butterworth_my.filter(my);
+      filt_mz = butterworth_mz.filter(mz);
+
+      Serial.print(".");
+
+      switch (sensor2use) {
+        case 'a':
+          {
+            tflInputTensor->data.f[numSamplesRead * 3 + 0] = filt_ax / 4.0;
+            tflInputTensor->data.f[numSamplesRead * 3 + 1] = filt_ay / 4.0;
+            tflInputTensor->data.f[numSamplesRead * 3 + 2] = filt_az / 4.0;
+            break;
+          }
+        case 'g':
+          {
+            tflInputTensor->data.f[numSamplesRead * 3 + 0] = filt_gx / 2000.0;
+            tflInputTensor->data.f[numSamplesRead * 3 + 1] = filt_gy / 2000.0;
+            tflInputTensor->data.f[numSamplesRead * 3 + 2] = filt_gz / 2000.0;
+            break;
+          }
+        case 'm':
+          {
+            tflInputTensor->data.f[numSamplesRead * 3 + 0] = filt_mx / 400.0;
+            tflInputTensor->data.f[numSamplesRead * 3 + 1] = filt_my / 400.0;
+            tflInputTensor->data.f[numSamplesRead * 3 + 2] = filt_mz / 400.0;
+            break;
+          }
+      }
+
+      numSamplesRead++;
+    }
+
+    // Do we have the samples we need?
+    if (numSamplesRead == NUM_SAMPLES) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("\nPrediction Probabilities");
+
+      // Run inference
+      TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+      if (invokeStatus != kTfLiteOk) {
+        Serial.println("Error: Invoke failed!");
+        while (1)
+          ;
+        return;
+      }
+
+      // Loop through the output tensor values from the model
+      int maxIndex = 0;
+      float maxValue = 0;
+      for (int i = 0; i < NUM_GESTURES; i++) {
+        float _value = tflOutputTensor->data.f[i];
+        if (_value > maxValue) {
+          maxValue = _value;
+          maxIndex = i;
+        }
+        Serial.print(GESTURES[i]);
+        Serial.print("\t: ");
+        Serial.println(tflOutputTensor->data.f[i], 6);
+      }
+
+      Serial.println("*******************************");
+      Serial.print("Ultimate Posture: ");
+      Serial.print(GESTURES[maxIndex]);
+      Serial.println();
+      Serial.println("*******************************");
+      digitalWrite(LED_BUILTIN, LOW);
+      numSamplesRead = 0;
+      isPredicting = false;
+    }
+  }
+
+  // Add delay to not double trigger
+  delay(1);
 }
